aed1d2f5bb1f7b74c6872ef9fb5edf3517f58795
eunjin0212 <eunjin9639@gmail.com>
2023-10-18 23:25:42 +0900

    🔍 sitemap, robots

---
+++ src/app/sitemap.ts
@@ -0,0 +1,24 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://gift-cert-eunjin0212.vercel.app/',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://gift-cert-eunjin0212.vercel.app/exchange',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://gift-cert-eunjin0212.vercel.app/notice',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+  ]
+}
