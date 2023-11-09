02cbbd99cf78fdca4068c2fc41365d2d35046b73
eunjin0212 <eunjin9639@gmail.com>
2023-11-09 22:42:32 +0900

    🎨 메인 배너 애니메이션 추가
    FIXME: 2번째 이미지 로드 안 됨

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -12,47 +12,49 @@ export default function Carousel() {
   const slideRef = useRef<HTMLDivElement>(null)
   const [index, setIndex] = useState(0)
 
-  useEffect(()=> {
-    setInterval(() => {
-      // console.log(index)
-      if(index === image.length) {
-        // setIndex(0)
-      } else {
-        // setIndex((idx) => idx + 1)
-      }
-    }, 1000)
-  }, [])
-  
   useEffect(() => {
-    slideRef.current!.style.transition = "all 0.5s ease-in-out";
-    slideRef.current!.style.transform = `translateX(-${index}00%)`; 
+    const timer = setInterval(() => {
+      if (index === image.length - 1) {
+        setIndex(0)
+        return
+      }
+      setIndex((idx) => idx + 1)
+    }, 3000)
+    return () => {
+      clearInterval(timer);
+    };
+  }, [index, image])
+
+  useEffect(() => {
+    slideRef.current!.style.transition = "all 1s ease-in-out";
+    slideRef.current!.style.transform = `translateX(-${index}00%)`;
   }, [index])
 
 
+  const iconClass = 'w-1/12 h-8 z-1 cursor-pointer absolute'
   return (
     <section className='w-full h-96 flex relative justify-between items-center'>
       <ChevronLeft
-        className='w-1/12 h-8 z-1 cursor-pointer'
+        className={[iconClass, 'left-0'].join(' ')}
         alt='left'
         width={30}
         height={30}
       />
-      <div className='h-96 w-auto flex overflow-hidden left-0' ref={slideRef}>
+      <div className='h-96 w-auto flex items-center overflow-hidden' ref={slideRef}>
         {image.map((img) => 
-          <Image
-            className='object-fill w-screen min-w-full h-96'
-            key={img.name}
-            alt={img.name}
-            width={1900}
-            height={384}
-            src={img.link}
-            placeholder='empty'
-            priority={true}
-          />
+          <div key={img.name} className='min-w-full w-screen h-96 relative'>
+            {/* FIXME: 2번째 이미지 로드 안 됨 */}
+            <Image
+              alt={img.name}
+              fill={true}
+              src={img.link}
+              priority={true}
+            />
+          </div>
         )}
       </div>
       <ChevronRight
-        className='w-1/12 h-8 z-1 cursor-pointer'
+        className={[iconClass, 'right-0'].join(' ')}
         alt='right'
         width={30}
         height={30}
